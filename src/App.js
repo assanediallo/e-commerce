e38bb8732517872306ad2c9c0f0ca16d1fff4749
Assane DIALLO <assane.diallo0612@gmail.com>
2022-07-22 10:45:10 +0200

    final version of the app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { commerce } from "./lib/vani-shop";
-import { Navbar, Products } from "./components";
+import { CssBaseline } from "@material-ui/core";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import { Navbar, Products, Cart } from "./components";
+import { commerce } from "./lib/commerce";
 
 const App = () => {
+  const [mobileOpen, setMobileOpen] = React.useState(false);
   const [products, setProducts] = useState([]);
   const [cart, setCart] = useState({});
 
@@ -16,10 +20,28 @@ const App = () => {
     setCart(await commerce.cart.retrieve());
   };
 
-  const handleAddToCart = async (productID, quantity) => {
-    const item = await commerce.cart.add(productID, quantity);
+  const handleAddToCart = async (productId, quantity) => {
+    const item = await commerce.cart.add(productId, quantity);
 
     setCart(item.cart);
+  };
+
+  const handleUpdateCartQty = async (lineItemId, quantity) => {
+    const response = await commerce.cart.update(lineItemId, { quantity });
+
+    setCart(response.cart);
+  };
+
+  const handleRemoveFromCart = async (lineItemId) => {
+    const response = await commerce.cart.remove(lineItemId);
+
+    setCart(response.cart);
+  };
+
+  const handleEmptyCart = async () => {
+    const response = await commerce.cart.empty();
+
+    setCart(response.cart);
   };
 
   useEffect(() => {
@@ -27,13 +49,43 @@ const App = () => {
     fetchCart();
   }, []);
 
-  console.log(cart);
+  console.log(products);
+
+  const handleDrawerToggle = () => setMobileOpen(!mobileOpen);
 
   return (
-    <div>
-      <Navbar totalItems={cart.total_items} />
-      <Products products={products} onAddToCart={handleAddToCart} />
-    </div>
+    <Router>
+      <div style={{ display: "flex" }}>
+        <CssBaseline />
+        <Navbar
+          totalItems={cart.total_items}
+          handleDrawerToggle={handleDrawerToggle}
+        />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Products
+                products={products}
+                onAddToCart={handleAddToCart}
+                handleUpdateCartQty
+              />
+            }
+          ></Route>
+          <Route
+            path="/cart"
+            element={
+              <Cart
+                cart={cart}
+                onUpdateCartQty={handleUpdateCartQty}
+                onRemoveFromCart={handleRemoveFromCart}
+                onEmptyCart={handleEmptyCart}
+              />
+            }
+          ></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -8,34 +8,39 @@ import {
   IconButton,
 } from "@material-ui/core";
 import { AddShoppingCart } from "@material-ui/icons";
+
 import useStyles from "./styles";
 
 const Product = ({ product, onAddToCart }) => {
   const classes = useStyles();
 
+  const handleAddToCart = () => onAddToCart(product.id, 1);
+
   return (
     <Card className={classes.root}>
       <CardMedia
         className={classes.media}
-        image={product.image}
+        image={product.image.url}
         title={product.name}
       />
       <CardContent>
-        <div className="classes.cardContent">
-          <Typography variant="h5" gutterBottom>
+        <div className={classes.cardContent}>
+          <Typography gutterBottom variant="h5" component="h2">
             {product.name}
           </Typography>
-          <Typography variant="h5">{product.price}</Typography>
+          <Typography gutterBottom variant="h5" component="h2">
+            ${product.price.formatted}
+          </Typography>
         </div>
-        <Typography variant="body2" color="textSecondary">
-          {product.description}
-        </Typography>
+        <Typography
+          dangerouslySetInnerHTML={{ __html: product.description }}
+          variant="body2"
+          color="textSecondary"
+          component="p"
+        />
       </CardContent>
-      <CardActions disableSpacing className={classes.CardActions}>
-        <IconButton
-          aria-label="Add to cart"
-          onClick={() => onAddToCart(product.id, 1)}
-        >
+      <CardActions disableSpacing className={classes.cardActions}>
+        <IconButton aria-label="Add to Cart" onClick={handleAddToCart}>
           <AddShoppingCart />
         </IconButton>
       </CardActions>
